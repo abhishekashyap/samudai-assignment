e703b4da8041e3d4108d7f4947bbe15edaaf5920
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-09-29 15:19:47 +0530

    feat(app): wrap app with ethers context

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { store } from "./app/store";
+import { EthersProvider } from "./context/EthereumSignInContext";
 import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,7 +13,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <EthersProvider>
+        <App />
+      </EthersProvider>
     </Provider>
   </React.StrictMode>
 );
