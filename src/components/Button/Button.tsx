6e3e8873d08885e883b4e6ac57fe5d1ce1912f6a
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-09-30 15:09:14 +0530

    refactor(button): implment classname and disabled props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,11 +4,16 @@ import { ButtonProps } from "./types";
 export const Button: FunctionComponent<ButtonProps> = ({
   children,
   onClick,
+  isDisabled = false,
+  className,
 }) => {
   return (
     <button
-      className="py-2 px-4 bg-transparent text-indigo-600 font-semibold border border-indigo-600 rounded hover:bg-indigo-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0"
+      className={`py-2 px-4 bg-transparent font-semibold text-gray-500 border  border-gray-500 rounded hover:bg-gray-500 hover:text-white hover:border-transparent transition ease-in duration-200 ${
+        className ? className : ""
+      }`}
       onClick={onClick}
+      disabled={isDisabled}
     >
       {children}
     </button>
