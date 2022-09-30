0a487e858b56f2a8c38e40eec3a0aaf84d861125
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-09-30 15:14:26 +0530

    feat(signIn): setWalletAddress in store and redirection

--- src/components/SignInWithEthereum/SignInWithEthereum.tsx
+++ src/components/SignInWithEthereum/SignInWithEthereum.tsx
@@ -1,10 +1,14 @@
-import { postSignInData } from "../../api";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { useEthers } from "../../context/EthereumSignInContext";
 import { createSiweMessage } from "../../utils";
 import Button from "../Button";
+import { setIsAuthenticated, setWalletAddress } from "./signInSlice";
 
 export const SignInWithEthereum = () => {
   const { provider } = useEthers();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const signer = provider.getSigner();
 
@@ -18,8 +22,18 @@ export const SignInWithEthereum = () => {
       "await signer.signMessage(message): ",
       await signer.signMessage(message)
     );
-    await postSignInData(signerAddress);
+    // await postSignInData(signerAddress);
+    dispatch(setWalletAddress(signerAddress));
+    dispatch(setIsAuthenticated(true));
+    navigate("/dashboard");
   }
 
-  return <Button onClick={signInWithEthereum}>Sign In With Ethereum</Button>;
+  return (
+    <Button
+      className="text-blue-500 border-blue-500 hover:bg-blue-500"
+      onClick={signInWithEthereum}
+    >
+      Sign In With Ethereum
+    </Button>
+  );
 };
