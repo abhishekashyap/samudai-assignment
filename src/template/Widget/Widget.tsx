3383886667cc2e4dcc7dec8a70effdb0dd4f0044
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-09-30 16:38:13 +0530

    refactor(widget): center align flex content

--- src/template/Widget/Widget.tsx
+++ src/template/Widget/Widget.tsx
@@ -5,15 +5,13 @@ export const Widget: FunctionComponent<WidgetProps> = ({
   className,
   header,
   children,
-}) => {
-  return (
-    <div
-      className={`h-72 w-72 p-6 flex flex-col rounded-xl hadow-xl absolute ${
-        className ? className : ""
-      }`}
-    >
-      <h3 className="mb-2 text-white font-semibold uppercase">{header}</h3>
-      <div className="flex-1 overflow-auto">{children}</div>
-    </div>
-  );
-};
+}) => (
+  <div
+    className={`h-72 w-72 p-6 flex flex-col rounded-xl shadow-xl absolute ${
+      className ? className : ""
+    }`}
+  >
+    <h3 className="mb-2 text-white font-semibold uppercase">{header}</h3>
+    <div className="flex flex-1 overflow-auto">{children}</div>
+  </div>
+);
