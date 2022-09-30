69402002924e8f08ea6776a0635131f77a75103a
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-09-30 15:12:42 +0530

    refactor(ConnectWallet): implement onConnect prop

--- src/components/ConnectWallet/ConnectWallet.tsx
+++ src/components/ConnectWallet/ConnectWallet.tsx
@@ -1,14 +1,26 @@
+import { FunctionComponent } from "react";
 import { useEthers } from "../../context/EthereumSignInContext";
 import Button from "../Button";
+import { ConnectWalletProps } from "./types";
 
-export const ConnectWallet = () => {
+export const ConnectWallet: FunctionComponent<ConnectWalletProps> = ({
+  onConnect,
+}) => {
   const { provider } = useEthers();
 
   function connectWallet() {
     provider
       .send("eth_requestAccounts", [])
+      .then(() => onConnect())
       .catch(() => console.error("User Rejected Request"));
   }
 
-  return <Button onClick={connectWallet}>Connect Wallet</Button>;
+  return (
+    <Button
+      className="text-yellow-600 border-yellow-600 hover:bg-yellow-600"
+      onClick={connectWallet}
+    >
+      Connect Wallet
+    </Button>
+  );
 };
