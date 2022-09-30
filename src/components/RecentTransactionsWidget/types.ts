1d9e9ab783a3d253970bd47fa973553b17121eb6
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-09-30 16:41:39 +0530

    feat(widget): add data structure for recent transaction widget

---
+++ src/components/RecentTransactionsWidget/types.ts
@@ -0,0 +1,28 @@
+export type RecentTransactionsResponseType = {
+  message: string;
+  result: TransactionType[];
+  status: string;
+};
+
+export type TransactionType = {
+  blockNumber: string;
+  timeStamp: string;
+  hash: string;
+  nonce: string;
+  blockHash: string;
+  transactionIndex: string;
+  from: string;
+  to: string;
+  value: string;
+  gas: string;
+  gasPrice: string;
+  isError: string;
+  txreceipt_status: string;
+  input: string;
+  contractAddress: string;
+  cumulativeGasUsed: string;
+  gasUsed: string;
+  confirmations: string;
+  methodId: string;
+  functionName: string;
+};
