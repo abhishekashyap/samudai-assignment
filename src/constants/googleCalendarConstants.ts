0f48439b63898da93850a25358d8451702dafbcb
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-09-30 15:18:10 +0530

    feat(constants): add google calendar props

---
+++ src/constants/googleCalendarConstants.ts
@@ -0,0 +1,8 @@
+export const DISCOVERY_DOC =
+  "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
+
+export const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+export const GOOGLE_API_URL = "https://apis.google.com/js/api.js";
+
+export const GSI_CLIENT_API_URL = "https://accounts.google.com/gsi/client";
