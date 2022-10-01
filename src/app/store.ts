35f60df4ab2527998b1a10247a782f205ae85680
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-10-01 15:44:09 +0530

    chore(store): add signin and etherscan reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,19 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { etherscanApiSlice } from "../api/etherscanApiSlice";
+import { signInApiSlice } from "../api/signInApiSlice";
 import signInReducer from "../components/SignInWithEthereum/signInSlice";
 
 export const store = configureStore({
   reducer: {
+    [signInApiSlice.reducerPath]: signInApiSlice.reducer,
+    [etherscanApiSlice.reducerPath]: etherscanApiSlice.reducer,
     signIn: signInReducer,
   },
+  // @ts-ignore multiple APIs not supported as types
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(signInApiSlice.middleware)
+      .concat(etherscanApiSlice.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
