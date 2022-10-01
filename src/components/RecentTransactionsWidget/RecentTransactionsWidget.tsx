9a31495119187f3122fb50c7bdf754b17116ec72
Abhishek Kashyap <abhishek.kashyap@medly.com>
2022-10-01 15:44:44 +0530

    chore(widget): implement rtk query hook in recent transaction widget

--- src/components/RecentTransactionsWidget/RecentTransactionsWidget.tsx
+++ src/components/RecentTransactionsWidget/RecentTransactionsWidget.tsx
@@ -1,35 +1,19 @@
-import { useEffect, useState } from "react";
+import { useGetTransactionListQuery } from "../../api/etherscanApiSlice";
 import { useAppSelector } from "../../app/hooks";
 import Widget from "../../template/Widget";
 import { selectWalletAddress } from "../SignInWithEthereum/signInSlice";
-import { RecentTransactionsResponseType, TransactionType } from "./types";
-
-const ETHERSCAN_API_KEY = process.env.REACT_APP_ETHERSCAN_API_KEY;
 
 export const RecentTransactionsWidget = () => {
   const currentWalletAddress = useAppSelector(selectWalletAddress);
 
-  const [transactionList, setTransactionList] = useState<TransactionType[]>([]);
-
-  async function getTransactionList(walletAddress: string) {
-    const txnListRes =
-      await fetch(`https://api.etherscan.io/api?module=account&action=txlist&address=${walletAddress}&startblock=0
-    &endblock=99999999&page=1&offset=10&sort=asc&apikey=${ETHERSCAN_API_KEY}`);
-
-    const txnList = (await txnListRes.json()) as RecentTransactionsResponseType;
-    setTransactionList(txnList.result);
-    return txnList;
-  }
-
-  useEffect(() => {
-    getTransactionList(currentWalletAddress);
-  }, [currentWalletAddress]);
+  const { data: transactionList = [], isLoading } =
+    useGetTransactionListQuery(currentWalletAddress);
 
   return (
     <Widget header="Recent Transactions" className="bg-purple-500 -left-52">
       {transactionList.length === 0 ? (
         <div className="flex flex-1 items-center justify-center text-purple-800 border-4 border-dashed border-purple-600 rounded font-semibold">
-          - No Recent Transactions -
+          {isLoading ? `Loading...` : `- No Recent Transactions -`}
         </div>
       ) : (
         <ul className="text-purple-200">
